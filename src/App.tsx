61a188ec02d5e97f5bf845f7afe1909bf2a95948
Kingchen711 <Kingchenobama711@gmail.com>
2023-09-11 21:13:54 +0700

    add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import {Button} from "@/components/ui/button";
+import {ModeToggle} from "./components/mode-toggle";
 
 function App() {
   return (
     <main>
+      <ModeToggle />
       <Button>Click Me</Button>
       <Button variant="secondary">Click Me</Button>
     </main>
