ecdc2e33d75a5f98916bc1f3f77ec5b409292a73
unknown <tuannnse170112@fpt.edu.vn>
2023-09-17 11:19:13 +0700

    Update event Picture and toggleList

--- src/pages/bird-detail.tsx
+++ src/pages/bird-detail.tsx
@@ -3,19 +3,34 @@ import { useNavigate, useParams } from 'react-router-dom'
 import Ellipce from '@/assets/Ellipse2.png'
 
 import { ChevronUp, ChevronDown, Container } from 'lucide-react'
+import { useRef, useState } from 'react'
 
 function BirdDetail() {
   const { id } = useParams()
   const navigate = useNavigate()
+  const imageRef = useRef<HTMLImageElement | null>(null);
+  const [isListVisible, setListVisible] = useState(false);
   if (!id) {
     navigate('/')
   }
+
+  const handleMouseEnter = (imageUrl: string) => {
+    if (imageRef.current) {
+      imageRef.current.src = imageUrl;
+    }
+  }
+
+  const toggleList = () => {
+    setListVisible(!isListVisible);
+  }
+
   return (
-    <main className=' bg-white mt-5 container sm:flex-row transition-all '>
+    <main className=' mt-5 container sm:flex-row transition-all '>
       <div className='flex justify-center  gap-5'>
         <div>
           <div>
-            <img          
+            <img
+              ref={imageRef}
               className='w-[500px] h-[450px] sm:w-100% object-cover'
               src='https://upload.wikimedia.org/wikipedia/commons/8/89/Black-naped_Oriole.jpg?fbclid=IwAR2NXjH7Wi1KWHPwvNvmESdLhjUE42zhr9Y-9KZneFisiKtkAimoH1ws8XI'
               alt='bird'
@@ -23,21 +38,33 @@ function BirdDetail() {
           </div>
           <div className='flex mt-5 gap-x-7 gap-auto flex-wrap w-full ml-2'>
             <img
+              onMouseEnter={() => {
+                handleMouseEnter('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKAKHac56qa2ePV7jdG_NGG1ExlwG8tpdzPLHQeKJxnVdj62ZmAfevw0v0G_tXFxSdFA0&usqp=CAU');
+              }}
               className='w-[100px]  object-cover'
               src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKAKHac56qa2ePV7jdG_NGG1ExlwG8tpdzPLHQeKJxnVdj62ZmAfevw0v0G_tXFxSdFA0&usqp=CAU'
               alt='bird'
             />
             <img
+              onMouseEnter={() => {
+                handleMouseEnter('https://upload.wikimedia.org/wikipedia/commons/8/89/Black-naped_Oriole.jpg?fbclid=IwAR2NXjH7Wi1KWHPwvNvmESdLhjUE42zhr9Y-9KZneFisiKtkAimoH1ws8XI');
+              }}
               className='w-[100px]  object-cover'
               src='https://upload.wikimedia.org/wikipedia/commons/8/89/Black-naped_Oriole.jpg?fbclid=IwAR2NXjH7Wi1KWHPwvNvmESdLhjUE42zhr9Y-9KZneFisiKtkAimoH1ws8XI'
               alt='bird'
             />
             <img
+              onMouseEnter={() => {
+                handleMouseEnter('https://upload.wikimedia.org/wikipedia/commons/8/89/Black-naped_Oriole.jpg?fbclid=IwAR2NXjH7Wi1KWHPwvNvmESdLhjUE42zhr9Y-9KZneFisiKtkAimoH1ws8XI');
+              }}
               className='w-[100px]  object-cover'
               src='https://upload.wikimedia.org/wikipedia/commons/8/89/Black-naped_Oriole.jpg?fbclid=IwAR2NXjH7Wi1KWHPwvNvmESdLhjUE42zhr9Y-9KZneFisiKtkAimoH1ws8XI'
               alt='bird'
             />
             <img
+              onMouseEnter={() => {
+                handleMouseEnter('https://upload.wikimedia.org/wikipedia/commons/8/89/Black-naped_Oriole.jpg?fbclid=IwAR2NXjH7Wi1KWHPwvNvmESdLhjUE42zhr9Y-9KZneFisiKtkAimoH1ws8XI');
+              }}
               className='w-[100px]  object-cover'
               src='https://upload.wikimedia.org/wikipedia/commons/8/89/Black-naped_Oriole.jpg?fbclid=IwAR2NXjH7Wi1KWHPwvNvmESdLhjUE42zhr9Y-9KZneFisiKtkAimoH1ws8XI'
               alt='bird'
@@ -93,86 +120,86 @@ function BirdDetail() {
 
       <div className='mt-3 p-3 transition-all'>
         <p
-          className='uppercase text-xl font-bold bg-[#1B1919] py-2 mb-3  text-white cursor-pointer'
-
+          className='uppercase text-xl font-bold bg-primary py-2 mb-3  text-white cursor-pointer'
+          onClick={toggleList}
         >
           <div className='flex gap-4 px-3 items-center'>
-            <ChevronUp></ChevronUp>
+            {(isListVisible) ? <ChevronUp></ChevronUp> : <ChevronDown></ChevronDown>}
             Mô tả chi tiết
           </div>
         </p>
 
-
-        <div>
-          <p>
-            Chim chào mào, còn được gọi là "Pycnonotus jocosus," là một loài chim thuộc họ Chim chào mào
-            (Pycnonotidae). Chúng là những loài chim nhỏ có âm thanh đặc biệt và có ngoại hình rất đẹp và phong cách.
-            Dưới đây là mô tả tổng quan về loài chim chào mào:
-          </p>
-          <ul className='list-decimal ml-6'>
-            <li>
-              Ngoại hình:
-              <ul style={{ listStyle: 'disc' }} className='ml-6'>
-                <li>Chim chào mào thường có kích thước nhỏ, dài khoảng 20-25 cm.</li>
-                <li>
-                  Chúng có màu lông rất đa dạng, nhưng phần lớn chúng có lông màu xanh dương, xám, trắng hoặc cam với
-                  các sọc đậm và họa tiết trên cánh và đuôi
-                </li>
-                <li>
-                  Mỏ của chim chào mào thường ngắn và hình nón, phù hợp cho việc ăn các loại trái cây và quả mọng.
-                </li>
-              </ul>
-            </li>
-            <li>
-              Ngoại hình:
-              <ul style={{ listStyle: 'disc' }} className='ml-6'>
-                <li>Chim chào mào thường có kích thước nhỏ, dài khoảng 20-25 cm.</li>
-                <li>
-                  Chúng có màu lông rất đa dạng, nhưng phần lớn chúng có lông màu xanh dương, xám, trắng hoặc cam với
-                  các sọc đậm và họa tiết trên cánh và đuôi
-                </li>
-                <li>
-                  Mỏ của chim chào mào thường ngắn và hình nón, phù hợp cho việc ăn các loại trái cây và quả mọng.
-                </li>
-              </ul>
-            </li>
-            <li>
-              Ngoại hình:
-              <ul style={{ listStyle: 'disc' }} className='ml-6'>
-                <li>Chim chào mào thường có kích thước nhỏ, dài khoảng 20-25 cm.</li>
-                <li>
-                  Chúng có màu lông rất đa dạng, nhưng phần lớn chúng có lông màu xanh dương, xám, trắng hoặc cam với
-                  các sọc đậm và họa tiết trên cánh và đuôi
-                </li>
-                <li>
-                  Mỏ của chim chào mào thường ngắn và hình nón, phù hợp cho việc ăn các loại trái cây và quả mọng.
-                </li>
-              </ul>
-            </li>
-            <li>
-              Ngoại hình:
-              <ul style={{ listStyle: 'disc' }} className='ml-6'>
-                <li>Chim chào mào thường có kích thước nhỏ, dài khoảng 20-25 cm.</li>
-                <li>
-                  Chúng có màu lông rất đa dạng, nhưng phần lớn chúng có lông màu xanh dương, xám, trắng hoặc cam với
-                  các sọc đậm và họa tiết trên cánh và đuôi
-                </li>
-                <li>
-                  Mỏ của chim chào mào thường ngắn và hình nón, phù hợp cho việc ăn các loại trái cây và quả mọng.
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-
+        {!isListVisible && (
+          <div>
+            <p>
+              Chim chào mào, còn được gọi là "Pycnonotus jocosus," là một loài chim thuộc họ Chim chào mào
+              (Pycnonotidae). Chúng là những loài chim nhỏ có âm thanh đặc biệt và có ngoại hình rất đẹp và phong cách.
+              Dưới đây là mô tả tổng quan về loài chim chào mào:
+            </p>
+            <ul className='list-decimal ml-6'>
+              <li>
+                Ngoại hình:
+                <ul style={{ listStyle: 'disc' }} className='ml-6'>
+                  <li>Chim chào mào thường có kích thước nhỏ, dài khoảng 20-25 cm.</li>
+                  <li>
+                    Chúng có màu lông rất đa dạng, nhưng phần lớn chúng có lông màu xanh dương, xám, trắng hoặc cam với
+                    các sọc đậm và họa tiết trên cánh và đuôi
+                  </li>
+                  <li>
+                    Mỏ của chim chào mào thường ngắn và hình nón, phù hợp cho việc ăn các loại trái cây và quả mọng.
+                  </li>
+                </ul>
+              </li>
+              <li>
+                Ngoại hình:
+                <ul style={{ listStyle: 'disc' }} className='ml-6'>
+                  <li>Chim chào mào thường có kích thước nhỏ, dài khoảng 20-25 cm.</li>
+                  <li>
+                    Chúng có màu lông rất đa dạng, nhưng phần lớn chúng có lông màu xanh dương, xám, trắng hoặc cam với
+                    các sọc đậm và họa tiết trên cánh và đuôi
+                  </li>
+                  <li>
+                    Mỏ của chim chào mào thường ngắn và hình nón, phù hợp cho việc ăn các loại trái cây và quả mọng.
+                  </li>
+                </ul>
+              </li>
+              <li>
+                Ngoại hình:
+                <ul style={{ listStyle: 'disc' }} className='ml-6'>
+                  <li>Chim chào mào thường có kích thước nhỏ, dài khoảng 20-25 cm.</li>
+                  <li>
+                    Chúng có màu lông rất đa dạng, nhưng phần lớn chúng có lông màu xanh dương, xám, trắng hoặc cam với
+                    các sọc đậm và họa tiết trên cánh và đuôi
+                  </li>
+                  <li>
+                    Mỏ của chim chào mào thường ngắn và hình nón, phù hợp cho việc ăn các loại trái cây và quả mọng.
+                  </li>
+                </ul>
+              </li>
+              <li>
+                Ngoại hình:
+                <ul style={{ listStyle: 'disc' }} className='ml-6'>
+                  <li>Chim chào mào thường có kích thước nhỏ, dài khoảng 20-25 cm.</li>
+                  <li>
+                    Chúng có màu lông rất đa dạng, nhưng phần lớn chúng có lông màu xanh dương, xám, trắng hoặc cam với
+                    các sọc đậm và họa tiết trên cánh và đuôi
+                  </li>
+                  <li>
+                    Mỏ của chim chào mào thường ngắn và hình nón, phù hợp cho việc ăn các loại trái cây và quả mọng.
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
 
       <div className='mt-3 p-3'>
-        <p className='uppercase text-xl font-bold bg-[#1B1919] py-2 mb-3  text-white cursor-pointer'>
+        <p className='uppercase text-xl font-bold bg-primary py-2 mb-3  text-white cursor-pointer'>
           <div className='flex gap-4 px-3'>Sản phẩm tương tự</div>
         </p>
       </div>
-      
+
     </main>
   )
 }
