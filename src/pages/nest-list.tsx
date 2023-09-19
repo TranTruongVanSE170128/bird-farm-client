6ed7ca31a5561dac25c1202c9d1c8732662f486c
Kingchen711 <Kingchenobama711@gmail.com>
2023-09-19 10:07:00 +0700

    fix nests before merge

--- src/pages/nest-list.tsx
+++ src/pages/nest-list.tsx
@@ -1,66 +1,53 @@
 import Container from '@/components/ui/container'
-import React from 'react'
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-    SelectGroup,
-    SelectLabel
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectGroup,
+  SelectLabel
+} from '@/components/ui/select'
+import NestCard from '@/components/nest-card'
 
-} from "@/components/ui/select"
-import { Button } from '@/components/ui/button'
-import NestList from '@/components/nest-list'
-type Props = {}
+function NestListItem() {
+  return (
+    <main>
+      <Container>
+        <div className='flex justify-between items-center mt-10 mb-6'>
+          <h1 className='text-3xl font-bold'>Các loài chim đang bán tại cửa hàng</h1>
+        </div>
 
-function NestListItem({ }: Props) {
+        <div className='pt-5 font-medium text-2xl'>Bộ Lọc</div>
 
-    return (
-        <main>
-            <Container>
+        <Select>
+          <SelectTrigger className='w-[180px] mt-3'>
+            <SelectValue className='font-semibold' placeholder='Lọc loài chim' />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Loài chim</SelectLabel>
+              <SelectItem value='Chim sẻ'>Chim sẻ</SelectItem>
+              <SelectItem value='Chim vành khuyên'>Chim vành khuyên</SelectItem>
+              <SelectItem value='Chim sáo'>Chim sáo</SelectItem>
+              <SelectItem value='Chim mật'>Chim mật</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
 
-                <div className='flex justify-center'>
-                    <p className='underline pt-5 font-medium text-4xl uppercase'>Tổ chim non có sẵn</p>
-                </div>
-                <div className=''>
-                    <div className='flex justify-end'>
-                        <p className='pt-5 font-medium text-2xl'>Loài Chim</p>
-                    </div>
-                    <div className='flex justify-end mt-3'>
-                        <Select>
-                            <SelectTrigger className="w-[180px]">
-                                <SelectValue className="font-semibold" placeholder="Chọn loài chim" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectGroup>
-                                    <SelectLabel>Loài chim</SelectLabel>
-                                    <SelectItem value="Chim sẻ">Chim sẻ</SelectItem>
-                                    <SelectItem value="Chim vành khuyên">Chim vành khuyên</SelectItem>
-                                    <SelectItem value="Chim sáo">Chim sáo</SelectItem>
-                                    <SelectItem value="Chim mật">Chim mật</SelectItem>
-                                </SelectGroup>
-                            </SelectContent>
-                        </Select>
-                    </div>
-                </div>
-                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8'>
-                    <NestList />
-                    <NestList />
-                    <NestList />
-                    <NestList />
-                    <NestList />
-                    <NestList />
-                    <NestList />
-                    <NestList />
-                </div>
-                <Button className='mx-auto block mt-6' size='lg'>
-                    Xem tất cả
-                </Button>
-
-            </Container>
-        </main>
-    )
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8'>
+          <NestCard />
+          <NestCard />
+          <NestCard />
+          <NestCard />
+          <NestCard />
+          <NestCard />
+          <NestCard />
+          <NestCard />
+        </div>
+      </Container>
+    </main>
+  )
 }
 
 export default NestListItem
