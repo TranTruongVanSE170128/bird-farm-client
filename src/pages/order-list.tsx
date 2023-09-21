1a8b32e484f44c7af731f736a224df3cc143d24e
unknown <tuannnse170112@fpt.edu.vn>
2023-09-21 19:33:07 +0700

    update status of orderList

--- src/pages/order-list.tsx
+++ src/pages/order-list.tsx
@@ -1,34 +1,61 @@
-import React, { useState, useRef, useEffect } from 'react';
-import Container from '@/components/ui/container';
+import React, { useState, useRef, useEffect } from 'react'
+import Container from '@/components/ui/container'
+import { Link, useLocation } from 'react-router-dom'
 
+type TabType = {
+  tabName: string
+  tabUrl: string
+}
+const tabItems: TabType[] = [
+  { tabName: 'Tất cả', tabUrl: 'all' },
+  { tabName: 'Đang xử lý', tabUrl: 'processing' },
+  { tabName: 'Đang giao', tabUrl: 'delivering' },
+  { tabName: 'Hoàn thành', tabUrl: 'success' },
+  { tabName: 'Đã hủy', tabUrl: 'cancel' }
+]
 function OrderList() {
-  const [activeTab, setActiveTab] = useState('Tất cả');
-  const tabItems = ['Tất cả', 'Đang xử lí', 'Đang giao', 'Hoàn thành', 'Đã hủy'];
-  const [barStyle, setBarStyle] = useState({ left: '0%', width: `${100 / tabItems.length}%` });
+  const location = useLocation()
+  const [activeTab, setActiveTab] = useState<TabType | null>(null)
+  const [barStyle, setBarStyle] = useState({ left: '0%', width: '0%' })
 
-  const tabRefs = useRef<(HTMLLIElement | null)[]>([]);
+  const tabRefs = useRef<(HTMLLIElement | null)[]>([])
+  const localStorageKey = 'activeTab'
 
-  
-  const handleTabClick = (tab: string, index: number) => {
-    setActiveTab(tab);
-    const tabWidthPercentage = 100 / tabItems.length;
-    const barPosition = index * tabWidthPercentage;
-    setBarStyle({ left: `${barPosition}%`, width: `${tabWidthPercentage}%` });
-  };
-
-
-  const handleTabMouseLeave = () => {
-    if (!tabRefs.current) return;
-    const activeTabIndex = tabItems.indexOf(activeTab);
-    const tabWidthPercentage = 100 / tabItems.length;
-    const barPosition = activeTabIndex * tabWidthPercentage;
-    setBarStyle({ left: `${barPosition}%`, width: `${tabWidthPercentage}%` });
-  };
-
- 
   useEffect(() => {
-    setActiveTab('Tất cả');
-  }, []);
+    const storedTab = localStorage.getItem(localStorageKey)
+    if (storedTab) {
+      const parsedTab = JSON.parse(storedTab)
+      setActiveTab(parsedTab)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (activeTab) {
+      localStorage.setItem(localStorageKey, JSON.stringify(activeTab))
+    }
+  }, [activeTab])
+
+  useEffect(() => {
+    const tabParam = new URLSearchParams(location.search).get('tab')
+    const matchedTab = tabItems.find((tab) => tab.tabUrl === tabParam)
+    if (matchedTab) {
+      setActiveTab(matchedTab)
+    }
+  }, [location.search, tabItems])
+
+  useEffect(() => {
+    if (activeTab) {
+      const activeTabIndex = tabItems.findIndex((tab) => tab.tabUrl === activeTab.tabUrl)
+      const tabWidthPercentage = 100 / tabItems.length
+      const barPosition = activeTabIndex * tabWidthPercentage
+      setBarStyle({ left: `${barPosition}%`, width: `${tabWidthPercentage}%` })
+    }
+  }, [activeTab, tabItems])
+
+  const handleTabClick = (tab: TabType, index: number) => {
+    setActiveTab(tab)
+    localStorage.setItem(localStorageKey, JSON.stringify(tab))
+  }
 
   return (
     <main>
@@ -38,23 +65,21 @@ function OrderList() {
           <ul className='flex justify-around h-full items-center'>
             {tabItems.map((tab, index) => (
               <li
-                key={tab}
+                key={tab.tabUrl}
                 className={`cursor-pointer hover:text-purple-500 ${
-                  activeTab === tab ? 'text-purple-500' : 'text-gray-600'
+                  activeTab && activeTab.tabUrl === tab.tabUrl ? 'text-purple-500' : 'text-gray-600'
                 }`}
                 ref={(el) => (tabRefs.current[index] = el)}
-                onClick={() => handleTabClick(tab, index)} 
-                onMouseLeave={handleTabMouseLeave} 
+                onClick={() => handleTabClick(tab, index)}
               >
-                {tab}
+                <Link to={`/orders?tab=${tab.tabUrl}`}>{tab.tabName}</Link>
               </li>
             ))}
           </ul>
         </div>
-       
       </Container>
     </main>
-  );
+  )
 }
 
-export default OrderList;
+export default OrderList
