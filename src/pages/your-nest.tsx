a248f395afe22d6f31f23bdc5df5f756ae3eb845
TruongDuyTven <duyvntse170126@fpt.edu.vn>
2023-09-25 22:38:50 +0700

    add process path

--- src/pages/your-nest.tsx
+++ src/pages/your-nest.tsx
@@ -5,7 +5,7 @@ import yourbird1 from '@/assets/yourbird1.jpg'
 import yourbird2 from '@/assets/yourbird2.jpg'
 import yourbird3 from '@/assets/yourbird3.jpg'
 import yourbird4 from '@/assets/yourbird4.jpg'
-import { type } from 'os'
+import { cn } from '@/lib/utils'
 
 const ListNest = [
   {
@@ -23,35 +23,77 @@ const ListNest = [
       gender: 'Cai',
       img: noImage
     },
-    stage1: yourbird1,
-    stage2: yourbird2,
-    stage3: yourbird3,
-    stage4: yourbird4
+    stage1: {
+      status: 'Trạng thái: Tốt',
+      numberOfBird: 'So luong: chua xac dinh',
+      img: yourbird1
+    },
+    stage2: {
+      status: 'Trạng thái: Tốt',
+      numberOfBird: 'So luong: 15',
+      img: yourbird2
+    },
+    stage3: {
+      status: 'Trạng thái: Tốt',
+      numberOfBird: 'So luong: 15',
+      img: yourbird3
+    },
+    stage4: {
+      status: 'Trạng thái: Tốt',
+      numberOfBird: 'So luong: 15',
+      img: yourbird4
+    }
+  },
+  {
+    dad: {
+      name: 'Chim chao mao Hue, ma SE170999',
+      specie: 'Chim chao mao',
+      type: 'chim phoi giong',
+      gender: 'Duc',
+      img: noImage
+    },
+    mom: {
+      name: 'Chim chao mao Hue, ma SE170999',
+      specie: 'Chim chao mao',
+      type: 'chim phoi giong',
+      gender: 'Cai',
+      img: noImage
+    },
+    stage1: {
+      status: 'Trạng thái: Tốt',
+      numberOfBird: 'So luong: chua xac dinh',
+      img: yourbird1
+    },
+    stage2: {
+      status: 'Trạng thái: Tốt',
+      numberOfBird: 'So luong: 15',
+      img: yourbird2
+    },
+    stage3: {
+      status: 'Trạng thái: Tốt',
+      numberOfBird: 'So luong: 15',
+      img: yourbird3
+    },
+    stage4: {
+      status: 'Trạng thái: Tốt',
+      numberOfBird: 'So luong: 15',
+      img: yourbird4
+    }
   }
-  // {
-  //     dad: {
-  //         name: 'Chim chao mao Hue',
-  //         specie: 'Chim chao mao',
-  //         type: 'chim phoi giong'
-  //     },
-  //     mom: {
-  //         name: 'Chim chao mao Hue',
-  //         specie: 'Chim chao mao',
-  //         type: 'chim phoi giong'
-  //     },
-  //     stage1: noImage,
-  //     stage2: noImage,
-  //     stage3: noImage,
-  //     stage4: noImage,
-  // }
 ]
 
 function YourNest() {
   const [selectImage, setSelectImage] = useState<string | undefined>(undefined)
-  const [indexButtonActive, setIndexButtonActive] = useState(-1);
+  const [indexButtonActive, setIndexButtonActive] = useState(-1)
+  
+  for (let i = 1; i <= ListNest.length; i++) {
+    const [selectImage, setSelectImage] = useState<string | undefined>(undefined)
+    const [indexButtonActive, setIndexButtonActive] = useState(-1)
+  }
 
   const handleStageClick = (image: string | undefined, index: number) => {
     setSelectImage(image)
+    setIndexButtonActive(index)
   }
 
   return (
@@ -66,8 +108,7 @@ function YourNest() {
             </div>
           ) : (
             ListNest.map((nest) => (
-              <div className='flex flex-col gap-5'>
-                <div className='w-full border-t border-foreground'></div>
+              <div className='flex flex-col gap-5 rounded-3xl border py-5 shadow-xl mb-10'>
                 <div className='flex'>
                   <div className='w-1/2 flex flex-col justify-center gap-5 mx-28'>
                     <div className='flex'>
@@ -93,37 +134,50 @@ function YourNest() {
                   </div>
                   {selectImage && <img className='h-40 w-64' src={selectImage} alt='Selected Image' />}
                 </div>
-                <div className='flex justify-around mt-10'>
-                  <div className='flex flex-col justify-center items-center gap-2'>
+
+                <div className='flex flex-wrap justify-between mt-10 relative w-full'>
+                  <div className='flex w-full justify-center border-t-2' />
+                  <div className='flex flex-col w-1/4 justify-center items-center gap-2 -mt-3'>
                     <button
-                      className='h-5 aspect-square rounded-full bg-foreground'
-                      onClick={() => handleStageClick(nest.stage1, 1)}
+                      className={cn(
+                        'h-5 aspect-square rounded-full',
+                        indexButtonActive === 1 ? 'bg-primary' : 'bg-foreground'
+                      )}
+                      onClick={() => handleStageClick(nest.stage1.img, 1)}
                     />
                     <div>Thanh cong phoi giong</div>
                   </div>
-                  <div className='flex flex-col justify-center items-center gap-2'>
+                  <div className='flex flex-col w-1/4 justify-center items-center gap-2 -mt-3'>
                     <button
-                      className='h-5 aspect-square rounded-full bg-foreground'
-                      onClick={() => handleStageClick(nest.stage2, 2)}
+                      className={cn(
+                        'h-5 aspect-square rounded-full',
+                        indexButtonActive === 2 ? 'bg-primary' : 'bg-foreground'
+                      )}
+                      onClick={() => handleStageClick(nest.stage2.img, 2)}
                     />
                     <div>Chim da de</div>
                   </div>
-                  <div className='flex flex-col justify-center items-center gap-2'>
+                  <div className='flex flex-col w-1/4 justify-center items-center gap-2 -mt-3'>
                     <button
-                      className='h-5 aspect-square rounded-full bg-foreground'
-                      onClick={() => handleStageClick(nest.stage3, 3)}
+                      className={cn(
+                        'h-5 aspect-square rounded-full',
+                        indexButtonActive === 3 ? 'bg-primary' : 'bg-foreground'
+                      )}
+                      onClick={() => handleStageClick(nest.stage3.img, 3)}
                     />
                     <div>To chim da no</div>
                   </div>
-                  <div className='flex flex-col justify-center items-center gap-2'>
+                  <div className='flex flex-col w-1/4 justify-center items-center gap-2 -mt-3'>
                     <button
-                      className='h-5 aspect-square rounded-full bg-foreground'
-                      onClick={() => handleStageClick(nest.stage4, 4)}
+                      className={cn(
+                        'h-5 aspect-square rounded-full',
+                        indexButtonActive === 4 ? 'bg-primary' : 'bg-foreground'
+                      )}
+                      onClick={() => handleStageClick(nest.stage4.img, 4)}
                     />
-                    <div>To chim sau x ngay</div>
+                    <div>To chim sau 15 ngay</div>
                   </div>
                 </div>
-                <div className='w-full border-t border-foreground'></div>
               </div>
             ))
           )}
