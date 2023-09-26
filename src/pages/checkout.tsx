9ffadeadabe53742a6b8ee52c77f7d1edc85052a
unknown <tuannnse170112@fpt.edu.vn>
2023-09-26 21:47:45 +0700

    update again checkout

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -37,33 +37,28 @@ type District = {
   isDeleted: false
 }
 const FormSchema = z.object({
-  type: z.enum(['cash', 'vnpay'], {
-    required_error: 'You need to select a notification type.'
+  type: z.enum(['cod', 'online'], {
+    required_error: 'Bạn cần lựa chọn phương thức thanh toán'
   }),
   firstName: z.string().min(2, {
-    message: 'Username must be at least 2 characters.'
+    message: 'Họ tối thiểu 2 chữ cái'
   }),
   lastName: z.string().min(2, {
-    message: 'Lastname must be at least 2 characters.'
+    message: 'Tên tối thiểu 2 chữ cái'
   }),
-  phoneNumber: z.string().min(11, {
-    message: 'PhoneNumber must be at least 11 characters.'
-  }),
-  province: z.string().min(11, {
-    message: 'Province not be blank.'
-  }),
-  district: z.string().min(11, {
-    message: 'District not be blank.'
-  }),
-  ward: z.string().min(11, {
-    message: 'Ward not be blank.'
-  }),
-  address: z.string().min(0, {
-    message: ''
-  }),
-  notice: z.string().min(0, {
-    message: ''
-  })
+  phoneNumber: z
+    .string()
+    .min(10, {
+      message: 'Số điện thoại có độ dài tối thiểu là 10 chữ số'
+    })
+    .max(11, {
+      message: 'Số điện thoại có độ dài tối đa là 11 chữ số'
+    }),
+  province: z.string().nonempty('Bắt buộc'),
+  district: z.string().nonempty('Bắt buộc'),
+  ward: z.string().nonempty('Bắt buộc'),
+  address: z.string().nonempty('Bắt buộc'),
+  notice: z.string().optional()
 })
 
 function Checkout() {
