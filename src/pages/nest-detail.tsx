4aaa8b8a4484d420b610dc445071cf7ebcf71b37
Kingchen711 <Kingchenobama711@gmail.com>
2023-09-26 16:40:49 +0700

    fix nest detail

--- src/pages/nest-detail.tsx
+++ src/pages/nest-detail.tsx
@@ -1,47 +1,71 @@
 import Container from '@/components/ui/container'
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import noImage from '@/assets/no-image.avif'
 import { formatPrice } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
 import { useCartContext } from '@/contexts/cart-provider'
 import { useToast } from '@/components/ui/use-toast'
 import Ellipce from '@/assets/Ellipse2.png'
+import { Nest, getSpecie } from '@/lib/types'
+import { birdFarmApi } from '@/services/bird-farm-api'
+import { Link, useParams } from 'react-router-dom'
+import Spinner from '@/components/ui/spinner'
+import NestCardSkeleton from '@/components/nest-card-skeleton'
+import NestCard from '@/components/nest-card'
 
 function NestDetail() {
   const imageRef = useRef<HTMLImageElement | null>(null)
   const { addNestToCart } = useCartContext()
   const { toast } = useToast()
+  const { id } = useParams()
+  const [nest, setNest] = useState<Nest | null>(null)
+
+  const [sameNests, setSameNests] = useState<Nest[]>([])
+  const [isLoadingSameNests, setIsLoadingSameNests] = useState(true)
+
   const handleMouseEnter = (imageUrl: string) => {
     if (imageRef.current) {
       imageRef.current.src = imageUrl
     }
   }
-  const nestList = {
-    id: '65100b5dd2122d65c01795fd',
-    name: 'Tổ Chim Bong Lau ma fcb34d',
-    specie: {
-      id: '65066ef9ecdd07bc4ale279c',
-      name: 'To chim Bong lau'
-    },
-    imageUrls: [
-      'https://i.ytimg.com/vi/TvDFssOoXyk/maxresdefault.jpg',
-      'https://images2.thanhnien.vn/Uploaded/2014/Picture20119/MinhNguyet/Thang9/chim.jpg',
-      'https://naidecor.vn/wp-content/uploads/2019/06/img_8834-copy.jpg',
-      'https://phathocdoisong.com/upload/image/201610/20161019102839_30484.jpg'
-    ],
-    sold: false,
-    price: 1045000,
-    description: 'To chim dang iu xinh dep nhat the gian'
+
+  useEffect(() => {
+    const fetchNest = async () => {
+      const { data } = await birdFarmApi.get(`/api/nests/${id}`)
+
+      setNest(data.nest || null)
+    }
+
+    fetchNest()
+  }, [id])
+
+  useEffect(() => {
+    const fetchSameNests = async () => {
+      if (!nest) return
+
+      const { data } = await birdFarmApi.get(
+        `/api/nests/pagination?pageNumber=1&pageSize=8&specie=${getSpecie(nest)._id}`
+      )
+
+      setSameNests(data.nests || [])
+      setIsLoadingSameNests(false)
+    }
+
+    fetchSameNests()
+  }, [nest])
+
+  if (!nest) {
+    return <Spinner className='mt-12' />
   }
+
   return (
     <main>
       <Container>
         <section className='grid grid-cols-12 gap-8 mt-10'>
           <div className='col-span-5'>
             <div>
-              {!nestList.imageUrls?.length ? (
+              {!nest.imageUrls?.length ? (
                 <img
-                  ref={imageRef}
                   className='w-full h-full aspect-square object-cover rounded cursor-pointer'
                   src={noImage}
                   alt='nest'
@@ -50,13 +74,13 @@ function NestDetail() {
                 <img
                   ref={imageRef}
                   className='w-full h-full aspect-square object-cover rounded cursor-pointer'
-                  src={nestList.imageUrls[0]}
+                  src={nest.imageUrls[0]}
                   alt='nest'
                 />
               )}
             </div>
             <div className='grid grid-cols-5 mt-2 gap-2 flex-wrap w-full'>
-              {nestList.imageUrls?.map((imageUrl) => {
+              {nest.imageUrls?.map((imageUrl) => {
                 return (
                   <img
                     onMouseEnter={() => {
@@ -72,25 +96,59 @@ function NestDetail() {
           </div>
 
           <div className='col-span-7 text-lg px-4'>
-            <h3 className='text-5xl font-semibold mb-6'>{nestList.name}</h3>
-            <div className='flex mb-6'>
-              <span className='font-medium mr-3 text-xl'>Danh mục: </span>
-              <span className=''>{nestList.specie.name}</span>
+            <h3 className='text-3xl font-semibold my-2'>{nest.name}</h3>
+
+            <div className='mt-4'>
+              <span className='font-medium'>Danh mục: </span>
+              <span>
+                <Link
+                  to={`/nests?specie=${getSpecie(nest)._id}`}
+                  className='font-normal hover:text-primary hover:underline cursor-pointer ml-2'
+                >
+                  Tổ {getSpecie(nest).name}
+                </Link>
+              </span>
             </div>
-            <div className='flex gap-4 items-center mb-6'>
-              <div className='font-medium text-xl mr-12 '>Giá:</div>
-              <p className='text-3xl text-red-500 font-medium'>{formatPrice(nestList.price)}</p>
+
+            <div className='flex gap-12 my-4'>
+              <div className='flex items-center gap-2 text-lg'>
+                <div className='font-medium'>Chim Bố:</div>{' '}
+                {nest.dad ? (
+                  <Link className='hover:underline hover:text-primary' to={`/admin/birds/${nest.dad._id}`}>
+                    {nest.dad.name}
+                  </Link>
+                ) : (
+                  'Không có thông tin'
+                )}
+              </div>
+
+              <div className='flex items-center gap-2 text-lg'>
+                <div className='font-medium'>Chim Mẹ:</div>{' '}
+                {nest.mom?._id ? (
+                  <Link className='hover:underline hover:text-primary' to={`/admin/birds/${nest.mom._id}`}>
+                    {nest.mom.name}
+                  </Link>
+                ) : (
+                  'Không có thông tin'
+                )}
+              </div>
             </div>
+
+            <div className='flex items-center gap-2'>
+              <div className='font-medium text-xl'>Giá: </div>
+              <p className='text-3xl text-red-500 font-medium'>{formatPrice(nest.price)}</p>
+            </div>
+
             <p className='my-3 mb-6'>
-              <span className='pr-1 font-medium mr-8 text-xl'>Mô tả :</span>{' '}
-              <span> {nestList.description || 'Không có thông tin'} </span>
+              <span className='pr-1 font-medium text-xl'>Mô tả :</span>{' '}
+              <span> {nest.description || 'Không có thông tin'} </span>
             </p>
 
             <div className='flex gap-4 my-6 mt-10'>
               <Button
                 className='text-lg'
                 onClick={() => {
-                  addNestToCart(nestList.id)
+                  addNestToCart(nest._id)
                   toast({
                     title: 'Đã thêm chim vào giỏ hàng',
                     variant: 'success'
@@ -117,6 +175,18 @@ function NestDetail() {
           >
             <img src={Ellipce} alt='' className='w-12 h-w-12' /> Hotline: 033233005
           </Button>
+        </div>
+
+        <div className='font-medium text-2xl my-4'>Sản phẩm tương tự</div>
+
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+          {isLoadingSameNests
+            ? Array(...new Array(8)).map((_, index) => {
+                return <NestCardSkeleton key={index} />
+              })
+            : sameNests?.map((nest) => {
+                return <NestCard key={nest._id} nest={nest} />
+              })}
         </div>
       </Container>
     </main>
