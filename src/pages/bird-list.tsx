b7348ce724a632efab5888ff5cdee034ff3af998
Kingchen711 <Kingchenobama711@gmail.com>
2023-10-12 21:59:32 +0700

    add filter and sort bird

--- src/pages/bird-list.tsx
+++ src/pages/bird-list.tsx
@@ -14,10 +14,17 @@ import {
   SelectValue
 } from '@/components/ui/select'
 import { Bird, Specie } from '@/lib/types'
-import { addSearchParams } from '@/lib/utils'
+import { addSearchParams, cn } from '@/lib/utils'
 import { birdFarmApi } from '@/services/bird-farm-api'
 import { useEffect, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
+import breedIcon from '@/assets/breed.svg'
+import birdIcon from '@/assets/bird-color.svg'
+import maleIcon from '@/assets/male.svg'
+import femaleIcon from '@/assets/female.svg'
+import { Button } from '@/components/ui/button'
+import { Calendar } from 'lucide-react'
+import decreaseIcon from '@/assets/decrease.svg'
 
 const pageSize = 12
 
@@ -26,11 +33,14 @@ function BirdList() {
   const pageNumber = Number(searchParams.get('pageNumber') || 1)
   const searchQuery = searchParams.get('searchQuery')
   const specie = searchParams.get('specie') || ''
-  const type = searchParams.get('type')
+  const gender = searchParams.get('gender') || ''
+  const type = searchParams.get('type') || ''
+  const sort = searchParams.get('sort') || 'createdAt_-1'
   const [birds, setBirds] = useState<Bird[]>([])
   const [isLoadingBirds, setIsLoadingBirds] = useState(true)
   const [totalPages, setTotalPages] = useState<number | null>(null)
   const [species, setSpecies] = useState<Specie[]>([])
+
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -51,7 +61,9 @@ function BirdList() {
           pageNumber,
           searchQuery,
           specie,
-          type
+          type,
+          gender,
+          sort
         })
       )
       setBirds(data?.birds || [])
@@ -60,7 +72,7 @@ function BirdList() {
     }
 
     fetchBirds()
-  }, [pageNumber, searchQuery, specie, type])
+  }, [pageNumber, searchQuery, specie, type, gender, sort])
 
   return (
     <main>
@@ -75,27 +87,141 @@ function BirdList() {
           </h1>
         </div>
 
-        <div className='text-2xl font-medium'>Bộ lọc loài chim</div>
+        <div className='text-2xl font-medium'>Chọn theo tiêu chí</div>
 
-        <Select
-          value={specie}
-          onValueChange={(value) => {
-            navigate(addSearchParams('/birds', { searchQuery, type, specie: value }))
-          }}
-        >
-          <SelectTrigger className='w-[180px] mt-3 mb-6'>
-            <SelectValue className='font-semibold' placeholder='Lọc loài chim' />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup className='h-96'>
-              <SelectLabel>Loài chim</SelectLabel>
-              <SelectItem value=''>Tất cả</SelectItem>
-              {species.map((specie) => {
-                return <SelectItem value={specie._id}>{specie.name}</SelectItem>
-              })}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
+        <div className='flex items-center mt-3 gap-4'>
+          <div className='flex items-center gap-2 bg-accent p-2 rounded-md border'>
+            <p className='font-medium text-sm shrink-0'>Loài chim:</p>
+            <Select
+              value={specie}
+              onValueChange={(value) => {
+                navigate(addSearchParams('/birds', { sort, searchQuery, type, specie: value, gender }))
+              }}
+            >
+              <SelectTrigger>
+                <SelectValue className='font-semibold' placeholder='Lọc loài chim' />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup className='h-96'>
+                  <SelectLabel>Loài chim</SelectLabel>
+                  <SelectItem value=''>Tất cả</SelectItem>
+                  {species.map((specie) => {
+                    return <SelectItem value={specie._id}>{specie.name}</SelectItem>
+                  })}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div className='flex items-center gap-2 bg-accent p-2 rounded-md border'>
+            <p className='font-medium text-sm shrink-0'>Giới tính:</p>
+            <Select
+              value={gender}
+              onValueChange={(value) => {
+                navigate(addSearchParams('/birds', { sort, searchQuery, type, specie, gender: value }))
+              }}
+            >
+              <SelectTrigger className=''>
+                <SelectValue className='font-semibold' placeholder='Lọc giới tính' />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup className=''>
+                  <SelectLabel>Giới tính</SelectLabel>
+                  <SelectItem className='cursor-pointer' value=''>
+                    Tất cả
+                  </SelectItem>
+                  <SelectItem className='cursor-pointer' value='male'>
+                    <div className='flex items-center'>
+                      Đực
+                      <img className='w-6 h-6 ml-1' src={maleIcon} alt='' />
+                    </div>
+                  </SelectItem>
+                  <SelectItem className='cursor-pointer' value='female'>
+                    <div className='flex items-center'>
+                      Cái
+                      <img className='w-6 h-6 ml-1' src={femaleIcon} alt='' />
+                    </div>
+                  </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div className='flex items-center gap-2 bg-accent p-2 rounded-md border'>
+            <p className='font-medium text-sm shrink-0'>Loại chim:</p>
+            <Select
+              value={type}
+              onValueChange={(value) => {
+                navigate(addSearchParams('/birds', { sort, searchQuery, type: value, specie, gender }))
+              }}
+            >
+              <SelectTrigger>
+                <SelectValue className='font-semibold' placeholder='Lọc loại chim...' />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup className=''>
+                  <SelectLabel>Loại Chim</SelectLabel>
+                  <SelectItem className='cursor-pointer' value=''>
+                    Tất cả
+                  </SelectItem>
+                  <SelectItem value='sell'>
+                    <div className='flex items-center'>
+                      <img className='w-6 h-6 mr-1' src={birdIcon} alt='' />
+                      Chim kiểng
+                    </div>
+                  </SelectItem>
+                  <SelectItem className='flex items-center' value='breed'>
+                    <div className='flex items-center'>
+                      <img className='w-6 h-6 mr-1' src={breedIcon} alt='' />
+                      Chim phối giống
+                    </div>
+                  </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+
+        <div className='text-2xl font-medium mt-3'>Sắp xếp theo</div>
+
+        <div className='mb-12 flex items-center mt-2 gap-3'>
+          <Button
+            onClick={() => {
+              navigate(addSearchParams('/birds', { sort: 'createdAt_-1', searchQuery, type, specie, gender }))
+            }}
+            variant={sort === 'createdAt_-1' ? 'default' : 'outline'}
+            className='flex items-center gap-1'
+          >
+            <Calendar className='w-5 h-5 mr-1' />
+            Mới nhất
+          </Button>
+          <Button
+            onClick={() => {
+              navigate(addSearchParams('/birds', { sort: 'price_1', searchQuery, type, specie, gender }))
+            }}
+            variant={sort === 'price_1' ? 'default' : 'outline'}
+            className='flex items-center gap-1'
+          >
+            <img
+              src={decreaseIcon}
+              className={cn('w-5 h-5 mr-1 dark:filter dark:invert scale-y-[-1]', sort === 'price_1' && 'filter invert')}
+            />
+            Giá tăng dần
+          </Button>
+          <Button
+            onClick={() => {
+              navigate(addSearchParams('/birds', { sort: 'price_-1', searchQuery, type, specie, gender }))
+            }}
+            variant={sort === 'price_-1' ? 'default' : 'outline'}
+            className='flex items-center gap-1'
+          >
+            <img
+              src={decreaseIcon}
+              className={cn('w-5 h-5 mr-1 dark:filter dark:invert', sort === 'price_-1' && 'filter invert')}
+            />
+            Giá giảm dần
+          </Button>
+        </div>
 
         {isLoadingBirds ? (
           <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
